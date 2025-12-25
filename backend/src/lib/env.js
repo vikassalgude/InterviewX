a69cf48a50048b7a45fc49075dd106fd0ae08304
VIKAS SALGUDE <vikassalgude04@gmail.com>
2025-12-25 12:57:03 +0530

    deployment config added

--- backend/src/lib/env.js
+++ backend/src/lib/env.js
@@ -2,5 +2,6 @@ import dotenv from 'dotenv';
 dotenv.config();
 export const ENV={
     PORT:process.env.PORT,
-    DB_URL:process.env.DB_URL
+    DB_URL:process.env.DB_URL,
+    NODE_ENV:process.env.NODE_ENV
 }
--- backend/src/server.js
+++ backend/src/server.js
@@ -1,11 +1,29 @@
 import express from 'express';
 import {ENV} from './lib/env.js'
-
+import path from 'path';
 const app=express();
-console.log(process.env.PORT)
-app.get('/',(req,res)=>{
-    res.status(200).json({msg:"success from api i"});
+console.log(process.env.PORT);
+
+const __dirname=path.resolve();
+
+
+app.get('/health',(req,res)=>{
+    res.status(200).json({msg:"api is up and running"});
 })
+
+app.get('/books',(req,res)=>{
+    res.status(200).json({msg:"books end point"});
+})
+
+//make our app forready or deployment
+
+if(ENV.NODE_ENV==='production'){
+   app.use(express.static(path.join(__dirname,"../frontend/dist")));
+   app.get('/{*any}',(req,res)=>{
+    res.sendFile(path.join(__dirname,"../frontend","dist","index.html"));
+   })
+}
+
 app.listen(ENV.PORT,()=>{
     console.log(`App is running on http://localhost:${ENV.PORT}`);
 })
