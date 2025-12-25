927eef581a9e0aff2fca3458596f95a2e93b5d29
VIKAS SALGUDE <vikassalgude04@gmail.com>
2025-12-25 16:57:40 +0530

    fix: add DB_URL validation in connectDB function

--- backend/src/lib/db.js
+++ backend/src/lib/db.js
@@ -3,6 +3,9 @@ import {ENV} from './env.js';
 
 export const connectDB=async()=>{
     try{
+        if(!ENV.DB_URL){
+            throw new Error("DB_URL is not defined in your enviroment varible")
+        }
         const conn=await mongoose.connect(ENV.DB_URL)
         console.log("connection successfull",conn.connection.host);  
     } 
